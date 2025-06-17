3b1e185f810923deb80fc6b39290ad7f6919b410
omerffae <omerekim825@gmail.com>
2025-06-17 15:00:21 +0300

    first commit

---
+++ src/components/modal/head.jsx
@@ -0,0 +1,23 @@
+import { useDispatch } from "react-redux";
+import c from "../../utils/nullCheck";
+import { close } from "../../redux/slices/detailSlice";
+
+const Head = ({ info }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <div className="head">
+      <div>
+        <h3 title="Çağrı İşareti">{c(info?.identification?.callsign)}</h3>
+
+        <span title="Uçuş Numarası">{c(info?.identification?.number?.default)}</span>
+
+        <span title="Uçak IATA/ICAO tip kodu">{c(info?.aircraft?.model?.code)}</span>
+      </div>
+
+      <button onClick={() => dispatch(close())}>X</button>
+    </div>
+  );
+};
+
+export default Head;
