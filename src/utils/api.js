57ce0ada96b56955a8beb6463f997fa126a06a02
omerffae <omerekim825@gmail.com>
2025-06-17 15:04:44 +0300

    .env added READMe.md update

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://flight-radar1.p.rapidapi.com",
   headers: {
-    "x-rapidapi-key": "75dc092df0msh3c03138e5cc1ea2p19035ejsn916bcc592247",
+    "x-rapidapi-key": import.meta.env.VITE_API_KEY,
     "x-rapidapi-host": "flight-radar1.p.rapidapi.com",
   },
 });
